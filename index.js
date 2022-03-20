7e6d1451fb25326accd876720d98135e98ae4954
Rishabh <r.gausai@gmail.com>
2022-03-20 08:22:41 +0530

    fix #8
    fixed issue unable to import puppeteer core

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const fs = require('fs');
 
 screenshot('https://example.com').then(() => console.log('screenshot saved'));
